b54aceac519175614c171d8593ae5c359bf8c4f7
naseer <naseer@naseer-Satellite-C850>
2016-09-17 18:19:54 -0500

    added view resolver to the MvcConfig file

--- gsu-smp/src/main/java/edu/gsu/smp/config/MvcConfig.java
+++ gsu-smp/src/main/java/edu/gsu/smp/config/MvcConfig.java
@@ -1,8 +1,11 @@
 package edu.gsu.smp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 @Configuration
@@ -11,7 +14,15 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-        // registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+    }
+    
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
     }
 
 }
