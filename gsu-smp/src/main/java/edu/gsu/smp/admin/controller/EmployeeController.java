c5ee7a232670485a81b993e4c7a3dc0817815baa
naseer <naseer@naseer-Satellite-C850>
2016-10-02 18:29:06 -0500

    redirecting to index page after adding employees

--- gsu-smp/src/main/java/edu/gsu/smp/admin/controller/EmployeeController.java
+++ gsu-smp/src/main/java/edu/gsu/smp/admin/controller/EmployeeController.java
@@ -2,10 +2,11 @@ package edu.gsu.smp.admin.controller;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
-import java.util.Random;
+import java.util.Arrays;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,20 +17,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.gsu.smp.entities.User;
+import edu.gsu.smp.service.UserService;
 
 @Controller
 @Secured({"ROLE_ADMIN"})
 @RequestMapping("/admin")
 public class EmployeeController {
 	
-	public EmployeeController() {
-		// TODO Auto-generated constructor stub
+	private UserService userService;
+	
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
 	}
 	
 	@RequestMapping(value="/create-employee", method=RequestMethod.GET)
 	@Secured({"ROLE_ADMIN", "ROLE_EMPLOYEE"})
 	public String createEmployee(Model model) {
 		User user = new User();
+		model.addAttribute("roles", Arrays.asList(User.Role.values()));
 		user.setPassword(new BigInteger(32, new SecureRandom()).toString(32));
 		model.addAttribute("user", user);
 		return "/admin/create-employee";
@@ -39,7 +45,17 @@ public class EmployeeController {
 	public String createEmployee(@ModelAttribute("user") @Valid User user,
 			BindingResult result, RedirectAttributes redirectAttributes) {
 		
-		return "";
+		user.setPassword(new BigInteger(32, new SecureRandom()).toString(32));
+		
+		System.out.println("this is your tmporary password: " + user.getPassword());
+		
+		if(result.hasErrors()) {
+			System.out.println("came here.........");
+			return "/admin/create-employee";
+		}
+		
+		userService.createEmployee(user);
+		return "redirect:/";
 	}
 
 }
