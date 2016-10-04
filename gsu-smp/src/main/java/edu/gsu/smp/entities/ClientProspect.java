2e48ddd56bf74ce37676c303f7dd447ee014fef4
naseer <naseer@naseer-Satellite-C850>
2016-10-04 17:43:52 -0500

    created ClientPropect Entity for the general details of a client

---
+++ gsu-smp/src/main/java/edu/gsu/smp/entities/ClientProspect.java
@@ -0,0 +1,254 @@
+package edu.gsu.smp.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name="client_prospect", indexes = {
+		@Index(columnList = "company_name", unique=true),@Index(columnList = "phone", unique=true)
+	})
+public class ClientProspect {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=1, max=User.NAME_MAX, message="{nameSizeError}")
+	@Column(nullable = false, length = User.NAME_MAX)
+	private String companyName;
+	
+	@NotNull
+	@Column(nullable = false, length = User.EMAIL_MAX)
+	private String email;
+	
+	@Column(nullable=true, length=15)
+	private String phone;
+	
+	@Column(nullable=true, length=24)
+	private String fax;
+	
+	@Column(nullable=true, length=64)
+	private String address;
+	
+	@Column(nullable=true, length=32)
+	private String city;
+	
+	@Column(nullable=true, length=16)
+	private String state;
+	
+	@Column(nullable=true, length=16)
+	private String country;
+	
+	@Column(nullable=true, length=12)
+	private String postal;
+	
+	@Column(nullable=true, length=32)
+	private String division;
+	
+	@Column(nullable=true, length=32)
+	private String industry;
+	
+	@Column(nullable=true, length=32)
+	private String type;
+	
+	@Column(nullable=true, length=64)
+	private String website;
+	
+	@Column(nullable=true, length=32)
+	private String source;
+	
+	@NotNull
+	@Size(min=10, max=2000, message="{descriptionSizeError}")
+	@Column(nullable=false, columnDefinition="text")
+	private String companyDescription;
+	
+	@NotNull
+	@Type(type="org.hibernate.type.NumericBooleanType")
+	@Column(nullable=false, name="is_client", columnDefinition="tinyint(1) default 0")
+	private boolean isClient;
+	
+	@NotNull
+	@Type(type="org.hibernate.type.NumericBooleanType")
+	@Column(nullable=false, name="is_prospect", columnDefinition="tinyint(1) default 0")
+	private boolean isProspect;
+	
+	@Column(nullable=true, name="date_created", columnDefinition="timestamp")
+	private Date dateCreated;
+	
+	@Column(nullable=true, name="last_updated", columnDefinition="datetime")
+	private Date lastUpdated = new Date();
+
+	// setters and getters
+	public long getId() {
+		return id;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getPostal() {
+		return postal;
+	}
+
+	public String getDivision() {
+		return division;
+	}
+
+	public String getIndustry() {
+		return industry;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public String getCompanyDescription() {
+		return companyDescription;
+	}
+
+	public boolean isClient() {
+		return isClient;
+	}
+
+	public boolean isProspect() {
+		return isProspect;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+
+	public void setDivision(String division) {
+		this.division = division;
+	}
+
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public void setCompanyDescription(String companyDescription) {
+		this.companyDescription = companyDescription;
+	}
+
+	public void setClient(boolean isClient) {
+		this.isClient = isClient;
+	}
+
+	public void setProspect(boolean isProspect) {
+		this.isProspect = isProspect;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+	
+}
