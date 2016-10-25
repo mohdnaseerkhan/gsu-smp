b584910afca2bf1c54cbf6f278f531b07500fca2
naseer <naseer@naseer-Satellite-C850>
2016-10-24 19:47:14 -0500

    allowed public access to about, contact page in the websecurityconfig.java

--- gsu-smp/src/main/java/edu/gsu/smp/config/WebSecurityConfig.java
+++ gsu-smp/src/main/java/edu/gsu/smp/config/WebSecurityConfig.java
@@ -52,6 +52,8 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .antMatchers("/",
+                		"/about",
+                		"/contact",
                 		"/home",
                 		"/error",
                 		"/signup",
